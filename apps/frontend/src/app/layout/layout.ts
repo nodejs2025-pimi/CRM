dec82f5d4841f40fe4f70cdeab9b31cec643ece1
Pavlo Satyrenko <zevsp09@gmail.com>
2025-09-26 16:31:23 +0300

    feat(frontend): implement sidebar component and improve layout component

--- apps/frontend/src/app/layout/layout.ts
+++ apps/frontend/src/app/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { Sidebar } from "@components/layout/sidebar/sidebar";
 
 @Component({
     selector: "app-layout",
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, Sidebar],
     templateUrl: "./layout.html",
     styleUrl: "./layout.css",
 })
