bb838abcf633c6162286a3b84c454d6810ad1f95
Pavlo Satyrenko <p.satyrenko@gmail.com>
2025-09-29 14:29:35 +0300

    feat(frontend): refactor layout structure and update routing path

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from "@angular/router";
 export const routes: Routes = [
     {
         path: "",
-        loadComponent: () => import("@layout/layout").then((m) => m.Layout),
+        loadComponent: () => import("@layouts/layout/layout").then((m) => m.Layout),
         children: [
             {
                 path: "analytics",
