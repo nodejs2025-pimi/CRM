2b86c378335564d52e1211d3c1bcd52e9584582a
Ilya <Bezzubik12345@i.ua>
2025-10-23 14:46:19 +0200

    feat(user): create admin user on startup

--- apps/backend/src/user/dtos/login-user.dto.ts
+++ apps/backend/src/user/dtos/login-user.dto.ts
@@ -1,22 +1,3 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { CredentialsDto } from './credentials.dto';
 
-export class LoginUserDto {
-  @ApiProperty({
-    minimum: 1,
-    maximum: 50,
-    example: 'makaka228',
-  })
-  @IsString()
-  @MinLength(1)
-  @MaxLength(50)
-  username: string;
-
-  @ApiProperty({
-    minimum: 8,
-    example: 'v3rY_Str0ng_p4$$w0rD',
-  })
-  @IsString()
-  @MinLength(8)
-  password: string;
-}
+export class LoginUserDto extends CredentialsDto {}
