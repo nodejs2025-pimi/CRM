d71fabb322bb9573c12eb5d45b6007144d78be32
PavloSatyrenko <zevsp09@gmail.com>
2025-10-27 23:36:15 +0200

    feat(interceptor): enhance HTTP interceptor to handle token refresh on 401 errors

--- apps/frontend/src/app/shared/interceptor/interceptor.ts
+++ apps/frontend/src/app/shared/interceptor/interceptor.ts
@@ -1,11 +1,36 @@
-import { HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { HttpErrorResponse, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { AuthService } from "@shared/services/auth.service";
+import { catchError, switchMap, throwError } from "rxjs";
 
 export const interceptor: HttpInterceptorFn = (req, next) => {
+    const authService: AuthService = inject(AuthService);
+
     const newRequest: HttpRequest<unknown> = req.clone({
         setHeaders: {
             Authorization: `Bearer ${localStorage.getItem("token") || ""}`,
         },
     });
-    
-    return next(newRequest);
+
+    return next(newRequest)
+        .pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error.status == 401) {
+                    const refreshToken: string | null = localStorage.getItem("refreshToken");
+
+                    if (refreshToken) {
+                        return authService.refreshToken(refreshToken)
+                            .pipe(
+                                switchMap(() => next(newRequest)),
+                                catchError((refreshError) => {
+                                    localStorage.removeItem("token");
+                                    return throwError(() => refreshError);
+                                })
+                            );
+                    }
+                }
+
+                return throwError(() => error);
+            })
+        );
 };
