6668cea06671cc8505f71b28ee24b6b8afb8ae50
PavloSatyrenko <zevsp09@gmail.com>
2025-10-27 18:42:32 +0200

    feat(ui): implement input and popup components with styles and functionality

---
+++ apps/frontend/src/app/shared/components/popup/popup.ts
@@ -0,0 +1,66 @@
+import { NgClass } from "@angular/common";
+import { Component, effect, ElementRef, HostListener, input, InputSignal, model, ModelSignal, output, OutputEmitterRef, Signal, viewChild } from "@angular/core";
+import { Button } from "@shared/components/button/button";
+import { Title } from "@shared/components/title/title";
+
+@Component({
+    selector: "ui-popup",
+    imports: [Title, Button, NgClass],
+    templateUrl: "./popup.html",
+    styleUrl: "./popup.css",
+})
+export class Popup {
+    public isOpen: ModelSignal<boolean> = model.required<boolean>();
+    public title: InputSignal<string> = input<string>("");
+
+    public closePopup: OutputEmitterRef<void> = output();
+
+    protected popupContent: Signal<ElementRef<HTMLDivElement>> = viewChild.required<ElementRef<HTMLDivElement>>("popupContent");
+
+    constructor() {
+        effect(() => {
+            const isPopupOpen: boolean = this.isOpen();
+
+            if (isPopupOpen) {
+                document.body.style.overflow = "hidden";
+            }
+            else {
+                document.body.style.overflow = "";
+            }
+        });
+    }
+
+    protected onCloseButtonClick(): void {
+        this.isOpen.set(false);
+
+        this.closePopup.emit();
+    }
+
+    @HostListener("document:click", ["$event"])
+    protected onBackdropClick(event: MouseEvent): void {
+        if (!this.isOpen()) {
+            return;
+        }
+
+        const targetNode: Node | null = event.target as Node | null;
+
+        if (targetNode && this.popupContent().nativeElement.contains(targetNode)) {
+            return;
+        }
+
+        this.onCloseButtonClick();
+    }
+
+    @HostListener("document:keydown", ["$event"])
+    protected onDocumentKeyDown(event: KeyboardEvent): void {
+        if (!this.isOpen()) {
+            return;
+        }
+
+        if (event.key === "Escape" || event.key === "Esc") {
+            event.preventDefault();
+
+            this.onCloseButtonClick();
+        }
+    }
+}
