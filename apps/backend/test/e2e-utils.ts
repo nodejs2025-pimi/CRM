1fa8c03fb4e7586da12f31756a247af09d21688b
IlliaSatyrenko <denzp05@gmail.com>
2025-11-27 16:16:13 +0200

    chore: add needed environment variable DB_HOST for docker

--- apps/backend/test/e2e-utils.ts
+++ apps/backend/test/e2e-utils.ts
@@ -20,6 +20,7 @@ export async function bootstrapApp(): Promise<{
   close: () => Promise<void>;
   server: Server;
 }> {
+  process.env.DB_HOST = process.env.DB_HOST_TEST ?? process.env.DB_HOST;
   process.env.DB_NAME = process.env.DB_NAME_TEST ?? process.env.DB_NAME;
   process.env.DB_USER = process.env.DB_USER_TEST ?? process.env.DB_USER;
   process.env.DB_PASSWORD =
